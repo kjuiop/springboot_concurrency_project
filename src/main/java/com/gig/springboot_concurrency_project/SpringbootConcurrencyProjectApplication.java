d6818a11a4cb16806c7369c4adc2cdaa112c45c7
kjuiop <copying_y@naver.com>
2022-03-15 11:22:46 +0900

    init commit

---
+++ src/main/java/com/gig/springboot_concurrency_project/SpringbootConcurrencyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gig.springboot_concurrency_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConcurrencyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConcurrencyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gig/springboot_concurrency_project/SpringbootConcurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gig.springboot_concurrency_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConcurrencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
